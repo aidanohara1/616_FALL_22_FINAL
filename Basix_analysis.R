efc9ced5ca17359ac7b0e60f5498a255ff94abb3
aidanohara1 <aohara1@bu.edu>
2022-12-16 18:06:06 -0500

    framework of slides and analysis

---
+++ Basix_analysis.R
@@ -0,0 +1,66 @@
+
+#  summer 2003, policy marketed to 2 villages
+#  using BASIXs best sales force
+#  148 famers purchased the policy#
+#  Paying 74000 Rs (1800$)
+#  for coverage of 600 total acres of land
+#  15% commision on sales, negligible revenue
+#  far outweighed by staff time, developing customer relations
+# 
+#  BASIX at large
+#  60000 borrowers
+#  in 4000 villages
+#  in 30 districts
+# 
+#  loan products typacally had an annual percentage rate of 24%
+#  primarily individual, sometimes grouped joint liabilities.
+#  average load sive of 8800 Rs,
+# 
+#  typically repaid in 11 to 24 months,
+#  monthly repayments matched to cash flow?
+#  crop loans mature after harvest
+#  small enterprise loans require regular month payments
+# 
+# 
+#  BASIX primary source of operating income is interest on the above loans
+#  SEE INCOME EXHIBIT 2
+#  9% cost of capitol
+#  2% provisioning for future loan losses
+#  FY 2003 expected long term loan loss rate was ~ 2%
+#  typical repayment rates for microfinance institutions ranged between 97% and 99%
+#  BASIX has a lower repayment rate then typical.
+#  Because of drought
+# 
+# 
+#  BASIX also sells life insurance,
+#  first to borrowers, bundled with the loan, 60000 borrowers
+#  policy covered 1.5 times the amount of the inital loan
+#  and eventually even to non-borrowers
+# 
+#  By 2004 BASIX is using 250 customer service agents
+#  serving exclusivley areas where commercial banks and microfinance coroprations
+#      wer not operating
+#  areas previously served by local moneylenders
+#   10% interest per month or higher
+#  no competetion for insurance sales for at least 5-10 years
+# 
+#  A BASIX client
+#  agriculture as a primary soucre of income in Andhra Pradesh (AP)
+#  a single household of ~5 members, a couple, a parent, and two children.
+#  ~3 acres of land owndership
+#  whereupon they would grow groupnuts or castor seed.
+#  3 acres of groud nut yeilded 1 ton (1000 kg) of seeds,
+#    which could be sold for 25-35 Rs / kg.
+#  middle men take substantial cut of profits
+#  households also have farm animals, grow food crops (maize or sorghum)
+#  expenses include farming inputs, seed and fertilizer, food, clothing, and fuel
+# 
+#  SEE CLIENTELE INCOME DISTRIBUTION IN EXHIBIT 3
+# 
+#  Farmers in AP and Tamil Nadu grow rice, groundnuts, sorghum, and more?
+#  limited to no irrigation
+#  rainfall levels are an important determinant of agricultural productivity
+#  The quality of monsoons is an actual determinant of economic growth nationwide
+#  two growing cycles
+#  Jun- Nov and Dec- Apr
+#  monsoon occurs during
